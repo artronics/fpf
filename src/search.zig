a0d1180b5fce5b253ce3a57b98549ef6096457af
artronics <jalalhosseiny@gmail.com>
2023-07-30 01:28:05 +0100

    add boundary

--- src/search.zig
+++ src/search.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const testing = std.testing;
 const expect = testing.expect;
 
@@ -9,21 +10,66 @@ pub const Search = struct {
         _ = self;
         var score = Score{};
 
+        // if (pattern.len == 0) return score;
+        // if (text.len == 0) return null;
+
         var i = text.len;
         var j = pattern.len;
         var last_pj: u8 = undefined;
+
+        var delete_acc: isize = 0;
+        var straight_acc: u5 = 0;
+
+        var boundary: ?[]const u8 = null;
+        var boundary_end: usize = undefined;
+        // var start_b: usize = undefined;
 
         while (i > 0 and j > 0) : (i -= 1) {
             const ti = text[i - 1];
             const pj = pattern[j - 1];
 
+            if (is_end_boundary(text, i - 1)) {
+                boundary_end = i;
+            }
+            if (is_start_boundary(text, i - 1)) {
+                boundary = text[i - 1 .. boundary_end];
+                std.log.warn("boundary: {?s}", .{boundary});
+            } else {
+                boundary = null;
+            }
+
             if (ti == pj) {
                 score.copy();
                 last_pj = pj;
+                straight_acc += 1;
+
+                if (boundary) |b| {
+                    delete_acc = 0;
+                    if (straight_acc != b.len) {
+                        score.boundary();
+                    }
+                }
+
                 j -= 1;
             } else if (last_pj == ti) {
                 score.copy();
+            } else {
+                delete_acc += 1;
+                // commit straight
+                score.straight(straight_acc);
+                straight_acc = 0;
             }
+
+            if (boundary) |_| {
+                score.delete(delete_acc);
+                delete_acc = 0;
+            }
+        }
+
+        if (j == 0) {
+            // commit what is left + kill
+            score.straight(straight_acc);
+            score.delete(delete_acc);
         }
 
         return if (j == 0) score else null;
@@ -33,11 +79,64 @@ pub const Search = struct {
         qc: isize = 1,
         _copy: isize = 0,
 
+        qd: isize = -1,
+        _delete: isize = 0,
+
+        qb: isize = -1,
+        _boundary: isize = 0,
+
+        _straight_acc: isize = 0,
+        inline fn qs(x: u5) isize {
+            // TODO: evaluate the below with a comparison test
+            // the score of 1 will be +2 which, combined by copy will contribute +3 in total. How this will impact short strings scoring?
+            // It feels like the score or 0 and 1 should be all 0?
+
+            // return if (x == 0 or x == 1) 0 else (@as(isize, 1) << (x + 1)) - 1;
+            return (@as(isize, 1) << (x + 1)) - 2;
+        }
+
         inline fn copy(self: *Score) void {
             self._copy += 1;
         }
+        inline fn delete(self: *Score, x: isize) void {
+            self._delete += x;
+        }
+        inline fn boundary(self: *Score) void {
+            self._boundary += 1;
+        }
+        inline fn straight(self: *Score, x: u5) void {
+            self._straight_acc += qs(x);
+        }
         inline fn score(self: Score) isize {
-            return self._copy * self.qc;
+            return self._copy * self.qc +
+                self._delete * self.qd +
+                self._boundary * self.qb +
+                self._straight_acc;
+        }
+        fn string(self: Score, allocator: Allocator) ![]u8 {
+            return std.fmt.allocPrint(allocator, "copy: {d}\ndelete: {d}\nboundary: {d}\nstraight_acc: {d}\nSCORE: {d}", .{ self._copy, self._delete, self._boundary, self._straight_acc, self.score() });
+        }
+
+        test "score" {
+            var s = Score{};
+
+            s.copy();
+            s.copy();
+            try expect(s._copy == 2);
+
+            s.delete(3);
+            try expect(s._delete == 3);
+
+            s.boundary();
+            s.boundary();
+            try expect(s._boundary == 2);
+
+            try expect(Score.qs(0) == 0);
+            s.straight(3);
+            try expect(s._straight_acc == qs(3));
+
+            const total = s.score();
+            try expect(total == s._copy * s.qc + s._delete * s.qd + s._boundary * s.qb + s._straight_acc);
         }
     };
 
@@ -131,6 +230,10 @@ test "match" {
 }
 
 test "score" {
+    var arena = std.heap.ArenaAllocator.init(testing.allocator);
+    defer arena.deinit();
+    const a = arena.allocator();
+
     var s = Search{};
 
     { // Copy
@@ -152,5 +255,44 @@ test "score" {
 
         r = try s.search("baxyax", "ba");
         try expect(r.?._copy == 3);
+    }
+    { // Straight
+        const qs = Search.Score.qs;
+
+        var r = try s.search("a", "a");
+        try expect(r.?._straight_acc == qs(1));
+
+        r = try s.search("?abc?", "abc");
+        try expect(r.?._straight_acc == qs(3));
+
+        r = try s.search("?abbbccc?", "abc");
+        try expect(r.?._straight_acc == qs(3));
+
+        r = try s.search("?ab?cde?", "abcde");
+        try expect(r.?._straight_acc == qs(2) + qs(3));
+    }
+    { // Delete
+        var r = try s.search("?axx", "a");
+        try expect(r.?._delete == 2);
+
+        r = try s.search("?bxxaxxx", "ba");
+        try expect(r.?._delete == 5);
+    }
+    { // Boundary aka Chunk
+        var r = try s.search("_axx", "a");
+        try expect(r.?._delete == 0);
+        try expect(r.?._boundary == 1);
+
+        r = try s.search("_axx_foo", "afoo");
+        std.log.warn("{s}", .{try r.?.string(a)});
+
+        try expect(r.?._delete == 0);
+        try expect(r.?._boundary == 1);
+
+        r = try s.search("_?ax_xbx", "ab");
+        std.log.warn("{s}", .{try r.?.string(a)});
+
+        try expect(r.?._delete == 4);
+        try expect(r.?._boundary == 0);
     }
 }
